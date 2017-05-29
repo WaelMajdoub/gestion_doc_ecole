999d8d6eb0498f53aa5ede890b5799bcb24b7f4f
stephanebressani <stephanebressani@192.167.1.111>
2017-05-28 19:03:16 -0700

    ok formulaire moche backend pure à 100%

--- src/Gde/GestionDocEcoleBundle/Controller/D01Controller.php
+++ src/Gde/GestionDocEcoleBundle/Controller/D01Controller.php
@@ -5,10 +5,64 @@ namespace Gde\GestionDocEcoleBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+//use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+//use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+use Symfony\Component\HttpFoundation\Request;
+
+use Gde\GestionDocEcoleBundle\Entity\D01Periode;
+use Gde\GestionDocEcoleBundle\Entity\D80Utilisateur;
+
 class D01Controller extends Controller
 {
-    public function nouveau_form_d01Action()
+    public function nouveau_form_d01Action(Request $request)
     {
-        return $this->render('GdeGestionDocEcoleBundle:D01:nouveau_form_d01.html.twig');
+        /*
+         * Toutes les saisies se font par l'utilisateur s.bressani@bluewin.ch
+         */
+        $repository = $this->getDoctrine()
+                            ->getManager()
+                            ->getRepository('GdeGestionDocEcoleBundle:D80Utilisateur');
+        $d80 = $repository->findOneBy(array('email' => 's.bressani@bluewin.ch'));
+        
+        $d01 = new D01Periode();
+        $d01->setId_d80($d80->getId());
+        $formBuilder = $this->createFormBuilder($d01);
+        $formBuilder->add('nom',        TextType::class)
+                    ->add('save',       SubmitType::class);
+        $form = $formBuilder->getForm();
+        
+        
+        // Si la requête est en POST
+        if ($request->isMethod('POST')) 
+        {
+            // On fait le lien Requête <-> Formulaire
+            // À partir de maintenant, la variable $advert contient les valeurs entrées dans le formulaire par le visiteur
+            $form->handleRequest($request);
+
+            // On vérifie que les valeurs entrées sont correctes
+            // (Nous verrons la validation des objets en détail dans le prochain chapitre)
+            if ($form->isValid()) 
+            {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($d01);
+                $em->flush();
+
+                $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+
+                // On redirige vers la page de visualisation de l'annonce nouvellement créée
+                //return $this->redirectToRoute('oc_platform_view', array('id' => $advert->getId()));
+            }
+        }
+        
+        return $this->render('GdeGestionDocEcoleBundle:D01:nouveau_form_d01.html.twig',array(
+            'form' => $form->createView(),));
     }
 }
